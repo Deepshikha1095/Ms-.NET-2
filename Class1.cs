e03f76502da64abf0a43926b58118f1686cc8864
Deepshikha1095 <deepshikha.kurre@gmail.com>
2024-11-03 09:29:55 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalLibrary
+{
+    public class EmployeeClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Dept { get; set; }
+    }
+    public class CDal
+    {
+        private readonly string cnnStr;
+        SqlConnection cnn;
+        SqlCommand cmd;
+
+        public CDal(string cnnstr)
+        {
+            this.cnnStr = cnnstr;
+            this.cnn = new SqlConnection(cnnstr);
+            this.cmd = new SqlCommand();
+            cmd.Connection = this.cnn;
+        }
+        public List<EmployeeClass> GetAllEmployees()
+        {
+            List<EmployeeClass> lstEmps = new List<EmployeeClass>();
+            cmd.CommandText = "select * from Employee";
+            cnn.Open();
+            SqlDataReader reader = this.cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                lstEmps.Add(new EmployeeClass { Id = (int)reader[0], Name = reader[1].ToString(), Dept = (int)reader[2] });
+            }
+            cnn.Close();
+            return lstEmps;
+        }
+        public EmployeeClass GetEmployeeById(int id)
+        {
+            EmployeeClass emp = null;
+            cmd.CommandText = $"select * from Employee where eid={id}";
+            cnn.Open();
+            SqlDataReader reader = this.cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                emp = new EmployeeClass { Id = (int)reader[0], Name = reader[1].ToString(), Dept = (int)reader[2] };
+            }
+            cnn.Close();
+            return emp;
+        }
+        public bool ModifyEmployee(EmployeeClass emp)
+        {
+            cmd.CommandText = $"update Employee set EName='{emp.Name}',Dept={emp.Dept} where Eid={emp.Id}";
+            cnn.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            cnn.Close();
+            return rowsAffected > 0;
+        }
+        public bool RemoveEmployee(int id)
+        {
+            cmd.CommandText = $"delete  Employee where Eid={id}";
+            cnn.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            cnn.Close();
+            return rowsAffected > 0;
+        }
+        public bool AddEmployee(EmployeeClass emp)
+        {
+            cmd.CommandText = $"insert into Employee values({emp.Id},'{emp.Name}',{emp.Dept})";
+            cnn.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            cnn.Close();
+            return rowsAffected > 0;
+        }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using DalLibrary;
+
+namespace MVC_Model.Controllers
+{
+    public class HomeController : Controller
+    {
+        static string str = @"Data Source=DESKTOP-RUT99G3\SQLEXPRESS01;Initial Catalog=DacWonders;Integrated Security=true";
+        CDal dal = new CDal(str);
+
+        public ActionResult Index()
+        {
+            return View(dal.GetAllEmployees());
+        }
+        [HttpGet]
+        public ActionResult AddEmp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEmp(EmployeeClass e)
+        {
+            dal.AddEmployee(e);
+            return RedirectToAction("Index");
+        }
+        public ActionResult UpdateEmployee(int id)
+        {
+            var emp = (from em in dal.GetAllEmployees() where em.Id == id select em).First();
+            return View(emp);
+
+        }
+        [HttpPost]
+        public ActionResult UpdateEmployee(EmployeeClass emp)
+        {
+            dal.ModifyEmployee(emp);
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteEmployee(int id)
+        {
+            dal.RemoveEmployee(id);
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
